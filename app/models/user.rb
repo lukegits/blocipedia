e39e9351874212b1f92b683a7c356fea5864c12e
Lukegits <luket.chavez@gmail.com>
2018-08-01 16:14:33 -0600

    call backs added to user and wiki models

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,14 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   # validates private: false
-  after_validation :wikis
+  after_update :publish_all
+   private
+    def publish_all
+     if @user.role == 'standard'
+       @user.wikis.where(private: true) do |makepub|
+         makepub.update_attributes(private: false)
+  end
+
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 
   has_many :collaborators
--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -1,6 +1,12 @@
 class Wiki < ActiveRecord::Base
+after_initialize :initialize_role
+
+   private
+   def initialize_role
+     self.private = false if self.private.nil?
+   end
+
   validates :title, presence: true
- before_validation private: false
   belongs_to :user, optional: true
   has_many :collaborators
   has_many :users, through: :collaborators
